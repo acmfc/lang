06cd4654c7348f719056a06edb003c03b8632f0c
acmfc <alan@themoys.org>
2017-01-16 20:51:52 -0700

    Add Type substitutions

--- src/Lang/Type.hs
+++ src/Lang/Type.hs
@@ -1,8 +1,11 @@
 module Lang.Type where
 
+import qualified Data.Map as Map
+
 type TypeVariableName = String
 data Type = TVar TypeVariableName
   | TCons String [Type]
+  deriving (Show, Eq)
 
 arrow :: Type -> Type -> Type
 arrow t1 t2 = TCons "arrow" [t1, t2]
@@ -13,6 +16,17 @@ int = TCons "int" []
 typeVariables :: Type -> [TypeVariableName]
 typeVariables t = typeVariables' t []
   where
-    typeVariables' (TVar x) accum = x:accum
-    typeVariables' (TCons x ts) accum = foldr typeVariables' accum ts
+    typeVariables' (TVar tvn) accum = tvn:accum
+    typeVariables' (TCons _ ts) accum = foldr typeVariables' accum ts
 
+type Subst = Map.Map TypeVariableName Type
+
+apply :: Subst -> Type -> Type
+apply sub (TVar tvn) = Map.findWithDefault (TVar tvn) tvn sub
+apply sub (TCons tcon ts) = TCons tcon (map (apply sub) ts)
+
+compose :: Subst -> Subst -> Subst
+compose sub1 sub2 = Map.map (apply sub1) sub2 `Map.union` sub1
+
+singleton :: TypeVariableName -> Type -> Subst
+singleton = Map.singleton
--- tests/Lang/Type/Tests.hs
+++ tests/Lang/Type/Tests.hs
@@ -1,14 +1,27 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module Lang.Type.Tests ( tests ) where
 
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (testCase)
 import Test.HUnit (Assertion, (@?=))
 
+import qualified Data.Map as Map
+
 import Lang.Type
 
 testTypeVariables :: Assertion
 testTypeVariables =
-  typeVariables (arrow (TVar "t1") (TVar "t2")) @?= ["t1", "t2"]
+  typeVariables (TVar "t1" `arrow` TVar "t2") @?= ["t1", "t2"]
+
+testCompose :: Assertion
+testCompose = apply (compose sub2 sub1) t @?= (apply sub2 . apply sub1) t
+ where
+  sub1 = Map.singleton "t3" (TVar "t4")
+  sub2 = Map.fromAscList [("t2", TVar "t5"), ("t6", TVar "t7")]
+  t = TVar "t1" `arrow` TVar "t6"
 
 tests :: TestTree
-tests = testGroup "Lang.Type" [testCase "testTypeVariables" testTypeVariables]
+tests = testGroup "Lang.Type"
+  [ testCase "testTypeVariables" testTypeVariables
+  , testCase "testCompose" testCompose
+  ]
