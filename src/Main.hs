6c52f55278aa704635f685a032d30d04598aa433
acmfc <alan@themoys.org>
2017-02-15 02:06:22 -0700

    Write demo program in main

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,25 @@
 module Main where
 
+import qualified Data.Map as Map
+
 import Lang.Core
+import Lang.DependencyAnalysis
+import Lang.Type
+
+initialTypeEnv :: TypeEnv
+initialTypeEnv =
+    TypeEnv $ Map.fromList [("+", toScheme (TFun TInt (TFun TInt TInt)))]
+
+program :: [BindingGroup]
+program = structureBindings
+    [ Binding { identifier="f", arguments=["a"],
+        body=EAp (EAp (EVar "+") (EVar "a")) (EAp (EVar "g") (ELit (LInt 0))) }
+    , Binding { identifier="g", arguments=["a"],
+        body=EAp (EVar "h") (EVar "a") }
+    , Binding { identifier="h", arguments=["a"],
+        body=EAp (EVar "g") (EVar "a") }
+    ]
 
 main :: IO ()
 main = do
-    putStrLn . show $ EVar "var"
+    putStrLn . show $ tiProgram initialTypeEnv program
